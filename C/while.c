7cbdf0f07bff584a39dd9911c4654f8666e23f1c
Bruna Barbieri <bruh_barbieri@hotmail.com>
2022-11-16 12:27:14 -0300

    incluindo aulas de C

---
+++ C/while.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int i = 0;
+while (i <= 50);
+{
+    printf("hello, world\n");
+    i++;
+}
+
+for (int i = 0; i < 50; i++);
+{
+    printf("hello, world\n");
+}
