102b69d64188102ace47d58b818bc5f214749cc1
Bruna Barbieri <bruh_barbieri@hotmail.com>
2022-11-24 12:46:15 -0300

    Incluindo aulas de Array

---
+++ Arrays/brick.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //char c1 = 'A';
+    //char c2 = 'B';
+    //char c3 = 'C';
+
+    string c = "ABC";
+
+    printf("%i %i %i\n", c[0], c[1], c[2]);
+    printf("%c %c %c\n", c[0], c[1], c[2]);
+}
