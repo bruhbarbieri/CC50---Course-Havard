c1f25d9ab1340da1b1e61fa54146f7bd043f8929
Bruna Barbieri <bruh_barbieri@hotmail.com>
2022-12-14 17:03:01 -0300

    Incluindo aulas de Memoria

---
+++ Memoria/copy2.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+//int main(void)
+//{
+//    char *s = get_string("s: ");
+//    char *t = get_string("t: ");
+//    if (s == t)
+//    {
+//         printf("Igual\n");
+//    }
+//    else
+//    {
+//         printf("Diferente\n");
+//    }
+//}
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+    if (strcmp(s, t) == 0)
+    {
+         printf("Igual\n");
+    }
+    else
+    {
+         printf("Diferente\n");
+    }
+}
