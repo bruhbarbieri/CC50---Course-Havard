bef61c7829a39035f4148b61d81465a258747484
Bruna Barbieri <bruh_barbieri@hotmail.com>
2023-05-03 19:39:51 -0300

    Incluindo atualizações de projeto

--- Projetos/BBB/application.py
+++ Projetos/BBB/application.py
@@ -1,57 +1,45 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request, session
-from flask_session import Session
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 db = SQL("sqlite:///bbb.db")
 
-SPORTS = [
-    "Dodgeball",
-    "Flag Football",
-    "Soccer",
-    "Volleyball",
-    "Ultimate Frisbee"
-]
+EDICOES = ["Todas", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
 
 @app.route("/")
 def index():
-    if not session.get("name"):
-        return redirect("/login")
-    return render_template("index.html", sports=SPORTS)
+    return render_template("index.html", edicoes=EDICOES)
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-   if request.method == "POST":
-        session["name"] = request.form.get("name")
-        return redirect("/")
-   return render_template("login.html")
+@app.route("/search", methods=["GET"])
+def search():
+    #nome = request.args.get("nome")
+    #if not nome:
+    #    return render_template("error.html", message="Missing name")
+    edicao = request.args.get("edicao")
+    nome = request.args.get("nome")
+    if not edicao:
+        return render_template("error.html", message="Missing editions")
+    if edicao not in EDICOES:
+        return render_template("error.html", message="Invalid Editions")
+    if edicao == "Todas":
+        if not nome:
+            participantes = db.execute("SELECT * FROM participantes")
+            return render_template("participantes.html", participantes=participantes)
 
-@app.route("/logout")
-def logout():
-   session["name"] = None
-   return redirect("/")
+        participantes = db.execute("SELECT * FROM participantes WHERE nome LIKE ?", "%" + nome + "%")
+        return render_template("participantes.html", participantes=participantes)
 
-@app.route("/register", methods=["POST"])
-def register():
-    name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
-    sport = request.form.get("sport")
-    if not sport:
-        return render_template("error.html", message="Missing sport")
-    if sport not in SPORTS:
-        return render_template("error.html", message="Invalid SportS")
 
-    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
+    participantes = db.execute("SELECT * FROM participantes WHERE edicoes_id = ?", edicao)
+    return render_template("participantes.html", participantes=participantes)
 
-    return redirect("/registrants")
-
-@app.route("/registrants")
-def registrants():
-    registrants = db.execute("SELECT * FROM registrants")
-    return render_template("registrants.html", registrants=registrants)
+#@app.route("/participantes", methods=["GET", "POST"])
+#def participantes(edicao):
+#    edicao = edicao
+#    participantes = db.execute("SELECT * FROM participantes WHERE edicoes_id = ?", edicao)
+#    return render_template("participantes.html", participantes=participantes)
 
 
