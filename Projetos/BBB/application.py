7c09786f6ad629a1b31206fb3fd44e52e3fc7c01
Bruna Barbieri <bruh_barbieri@hotmail.com>
2023-05-07 20:12:45 -0300

    Incluindo atualizações de projeto

--- Projetos/BBB/application.py
+++ Projetos/BBB/application.py
@@ -7,36 +7,38 @@ app = Flask(__name__)
 
 db = SQL("sqlite:///bbb.db")
 
-EDICOES = ["Todas", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+EDICOES = db.execute("SELECT edicoes_id FROM participantes GROUP BY edicoes_id")
 
-@app.route("/")
+SEXOS = db.execute("SELECT sexo FROM participantes GROUP BY sexo")
+
+COLOCACOES = db.execute("SELECT colocacao FROM participantes GROUP BY colocacao")
+
+ESTADOS = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RR", "RO", "RJ", "RN", "RS", "SC", "SP", "SE", "TO", "EXTERIOR"]
+
+SIGNOS = db.execute("SELECT signo FROM participantes GROUP BY signo")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", edicoes=EDICOES)
+    #participantes=request.args.get("participantes")
+    return render_template("index.html")
 
-@app.route("/search", methods=["GET"])
+@app.route("/searchparticipantes", methods=["GET", "POST"])
+def searchparticipantes():
+        return render_template("participantesearch.html", edicoes=EDICOES, sexos=SEXOS, colocacoes=COLOCACOES, estados=ESTADOS, signos=SIGNOS)
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     edicao = request.args.get("edicao")
     nome = request.args.get("nome")
-    if not edicao:
-        return render_template("error.html", message="Missing editions")
-    if edicao not in EDICOES:
-        return render_template("error.html", message="Invalid Editions")
-    if edicao == "Todas":
-        if not nome:
-            participantes = db.execute("SELECT * FROM participantes")
-            return render_template("participantes.html", participantes=participantes)
+    sexo = request.args.get("sexo")
+    profissao = request.args.get("profissao")
+    cidade = request.args.get("cidade")
+    colocacao = request.args.get("colocacao")
+    estado = request.args.get("estado")
+    signo = request.args.get("signo")
 
-        participantes = db.execute("SELECT * FROM participantes WHERE nome LIKE ?", "%" + nome + "%")
-        return render_template("participantes.html", participantes=participantes)
-
-
-    participantes = db.execute("SELECT * FROM participantes WHERE edicoes_id = ? AND nome LIKE ?", edicao, "%" + nome + "%")
+    participantes = db.execute("SELECT * FROM participantes WHERE edicoes_id LIKE ? AND nome LIKE ? AND sexo LIKE ? AND profissao LIKE ? AND cidade LIKE ? AND colocacao LIKE ? AND estado LIKE ? AND signo LIKE ?", edicao, "%" + nome + "%", sexo, "%" + profissao + "%", "%" + cidade + "%", colocacao, estado, signo)
     return render_template("participantes.html", participantes=participantes)
 
-#@app.route("/participantes", methods=["GET", "POST"])
-#def participantes(edicao):
-#    edicao = edicao
-#    participantes = db.execute("SELECT * FROM participantes WHERE edicoes_id = ?", edicao)
-#    return render_template("participantes.html", participantes=participantes)
 
 
